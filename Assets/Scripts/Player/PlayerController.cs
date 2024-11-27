c5d84332db234c022778c296c0af953013222512
Ramon Vallbona Degollada <rvallbona2001@gmail.com>
2024-11-27 18:18:07 +0100

    Update movement with mobilecontroller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,130 +5,108 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    bool canJump;
-    public float forceRight = 800f;
-    public float forceLeft = -800f;
-    public float forceJump = 300f;
-
     public float forceBoost = 800f;
 
-    //Disparo Player
+    #region PlayerShoot
     public Transform PlayerFirePoint;
     public GameObject PlayerBullet;
-
-    //Vida
+    #endregion
+    #region hp
     public float HPmax;
     public float CurrentHP;
     public Slider healthSlider;
     public int CuraVida;
-
-    Animator anim;
-
+    #endregion
     bool pause;
 
-    //public GameObject PauseMenu;
+    #region PlayerControllers
+    [SerializeField] Joystick joystick;
 
+    [SerializeField] private float runSpeedHorizontal = 3, runSpeedVertical = 3, runSpeed = 0, jumpForce = 2;
+    private float horizontalMove = 0, verticalMove = 0;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    #endregion
     void Start()
     {
         CurrentHP = HPmax;
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        MoveLeftRight();
-        Jump();
+        if (joystick.Vertical >.5f && isGrounded)
+        {
+            Jump();
+            isGrounded = false;
+        }
+        verticalMove = joystick.Vertical * runSpeedVertical; horizontalMove = joystick.Horizontal * runSpeedHorizontal;
+        transform.position += new Vector3(horizontalMove, 0, 0) * Time.deltaTime * runSpeed;
+
+
+
+
+
+
+
         Shoot();
         ComprobarHP();
-        //Pause();
     }
-    void MoveManual()
+    private void Jump()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            gameObject.transform.Translate(-30f * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            gameObject.transform.Translate(30f * Time.deltaTime, 0, 0);
-        }
-        ManageJump();
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
-    void ManageJump()
-    {
-        if (gameObject.transform.position.y <= 0)
-        {
-            canJump = true;
-        }
-        if (Input.GetKey(KeyCode.Space) && canJump && gameObject.transform.position.y < 10)
-        {
-            gameObject.transform.Translate(0, 100f * Time.deltaTime, 0);
-        }
-        else
-        {
-            canJump = false;
 
-            if (gameObject.transform.position.y > 0)
-            {
-                gameObject.transform.Translate(0, -30f * Time.deltaTime, 0);
-            }
-        }
-    }
-    void MoveLeftRight()
-    {
-        if (Input.GetKey(KeyCode.A))//LEFT
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(forceLeft * Time.deltaTime, 0));
-            gameObject.GetComponent<Animator>().SetBool("Run", true);//animation
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-        if (Input.GetKey(KeyCode.D))//RIGHT
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(forceRight * Time.deltaTime, 0));
-            gameObject.GetComponent<Animator>().SetBool("Run", true);///animation
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-        //animation
-        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
-        {
-            gameObject.GetComponent<Animator>().SetBool("Run", false);
-        }
-        //animation
-    }
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)//GetKeyDown imortante para que no se tenga que mantener pulsado
-        {
-            canJump = false;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceJump));//el 500f es la fuerza del salto.
-        }
-        //animation
-        if (!canJump)
-        {
-            gameObject.GetComponent<Animator>().SetBool("Jump", true);
-            gameObject.GetComponent<Animator>().SetBool("Run", false);
-        }
-        if (canJump)
-        {
-            gameObject.GetComponent<Animator>().SetBool("Jump", false);
-        }
-        //animation
-    }
-    //SOLUCIONAR PROBLEMA DEL SALT!
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Grounded")
         {
-            canJump = true;
+            isGrounded = true;
         }
         if (collision.transform.tag == "EnemyFly")
         {
             PlayerHit(5);
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void ManageShot()
+    {
+        Instantiate(PlayerBullet, PlayerFirePoint.position, PlayerFirePoint.rotation);
+    }
     void Shoot()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            Instantiate(PlayerBullet, PlayerFirePoint.position, PlayerFirePoint.rotation);
+            ManageShot();
         }
     }
     public void PlayerHit(int daño)
@@ -166,7 +144,6 @@ public class PlayerController : MonoBehaviour
         //PauseMenu.SetActive(true);
         //pause = true;
     }
-
     public void ClosePauseMenu()
     {
         GameManager.Instance.ResumeGame();
